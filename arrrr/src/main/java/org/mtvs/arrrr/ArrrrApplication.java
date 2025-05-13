233b750eb52d4bf4b7b30bd58a3b9727d51713b4
jake <jktom@gmail.com>
2025-05-13 21:51:17 +0900

    initial commit

---
+++ arrrr/src/main/java/org/mtvs/arrrr/ArrrrApplication.java
@@ -0,0 +1,13 @@
+package org.mtvs.arrrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrrrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrrrApplication.class, args);
+    }
+
+}
---
+++ arrrr/src/test/java/org/mtvs/arrrr/ArrrrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mtvs.arrrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrrrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
